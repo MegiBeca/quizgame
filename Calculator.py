03397a807ec9891d6eff5a76344765c63ed2e6b4
MegiBeca <54412551+MegiBeca@users.noreply.github.com>
2019-11-20 17:33:44 -0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,3 @@
+groceryList = int(input("Please enter you grocery list.")
+fruit = {"bannana": 1.25,"strawberries": 5.50,"cherries": 4.00,"grapes": 5.00,"peaches": 2.00,"apples": 1.00}
+vegtables = {"carrets": 4.25,"lettis": 2.25,}
